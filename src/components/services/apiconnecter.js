dc1b7798d3fbbe422a8ed8f251b075bcfcb8fb6d
Manish Bhartiyg <bhartimanish435@gmail.com>
2025-07-13 12:54:29 +0530

    deployurl

--- src/components/services/apiconnecter.js
+++ src/components/services/apiconnecter.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({});
-// const Base_Url = "https://chatsapp-server-5.onrender.com/api/v1/";
-const Base_Url = "http://localhost:4000/api/v1/";
+const Base_Url = "https://chat-app-back-ten.vercel.app/api/v1/";
+// const Base_Url = "http://localhost:4000/api/v1/";
 export const apiConnecter = (method, url, bodyData, headers, params) => {
     console.log(Base_Url,url);
     return axiosInstance({
